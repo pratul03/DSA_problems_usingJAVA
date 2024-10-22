fceeba9b861f6c54a4eb5eba0ab5e270675d880c
Pratul Makar <pratulmakar02@gmail.com>
2024-10-23 00:07:14 +0530

    updated

---
+++ RecursiveBinarySearch.java
@@ -0,0 +1,30 @@
+public class RecursiveBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int target = 5;
+
+        int result = binarySearch(arr, target, 0, arr.length - 1);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target, int left, int right) {
+
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            return binarySearch(arr, target, mid + 1, right);
+        } else {
+            return binarySearch(arr, target, left, mid - 1);
+        }
+    }
+}
